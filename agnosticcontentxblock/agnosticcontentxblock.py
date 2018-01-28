e5032d5f6e4362ccdb8931be8362d05550d80f16
renevatium <rene@proversity.org>
2018-01-28 16:18:52 +0200

    initial vertical student view for bookmarks

--- agnosticcontentxblock/agnosticcontentxblock.py
+++ agnosticcontentxblock/agnosticcontentxblock.py
@@ -167,8 +167,10 @@ class AgnosticContentXBlock(StudioContainerWithNestedXBlocksMixin, StudioEditabl
 		}))
 
 		fragment.add_css(self.resource_string("static/css/agnosticcontentxblock.css"))
+		fragment.add_javascript_url(self.runtime.local_resource_url(self, 'public/js/vertical_student_view.js'))
 		fragment.add_javascript_url(self.runtime.local_resource_url(self, 'public/js/agnosticcontentxblock.js'))
 		fragment.initialize_js('AgnosticContentXBlock')
+		fragment.initialize_js('VerticalStudentView')
 		return fragment
 
 
